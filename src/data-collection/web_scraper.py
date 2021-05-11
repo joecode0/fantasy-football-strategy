30bd39f7ba6c857e0889081f17080ec7b02444e0
joecode0 <joecode0@outlook.com>
2021-05-11 14:15:27 +0100

    Fix .gitignore

--- src/data-collection/web_scraper.py
+++
@@ -1,9 +0,0 @@
-from proxy_requests import ProxyRequests
-import requests
-from bs4 import BeautifulSoup as soup
-
-def get_raw_html(web_link):
-    r = ProxyRequests(web_link)
-    r.get()
-    page = soup(r.get_raw(), "html.parser")
-    return page
